f31c6fd5ad0a076c132c2b9d3b1e1d8ba83c41b2
gordienko <tolig.emale@gmail.com>
2018-01-15 16:01:39 +0200

    fixed data typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,30 +1,46 @@
+/* tslint:disable:member-access */
 import * as React from 'react';
 
 
 declare module 'material-ui-breadcrumbs' {
-  export interface BreadcrumbStep {
-    label: string;
-    uri: string;
-  }
-
-  export interface Breadcrumbs {
-    className?: string;
-    style?: {};
-    itemClassName?: string;
-    itemStyle?: {};
-    activeItemClassName?: string;
-    activeItemStyle?: {};
-    separatorClassName?: string;
-    separatorStyle?: {};
-    separator?: React.ReactElement<any>;
-    transparentBackground?: boolean;
-    customBackgroundClassName?: string;
-    customBackgroundStyle?: {};
-    customBackground?: React.ReactElement<any>;
-    currentStep: string;
-    steps: {
+  export import Breadcrumbs = __components.Breadcrumbs;
+  export import BreadcrumbStep = __components.BreadcrumbStep;
+  export import BreadcrumbsProps = __components.BreadcrumbsProps;
+  export import BreadcrumbsContext = __components.BreadcrumbsContext;
+  export default {
+    Breadcrumbs, BreadcrumbStep, BreadcrumbsProps, BreadcrumbsContext,
+  } as {
+    Breadcrumbs, BreadcrumbStep, BreadcrumbsProps, BreadcrumbsContext,
+  };
+  declare namespace __components {
+    export interface BreadcrumbStep {
       label: string;
       uri: string;
-    }[];
+    }
+    export interface BreadcrumbsProps {
+      className?: string;
+      style?: {};
+      itemClassName?: string;
+      itemStyle?: {};
+      activeItemClassName?: string;
+      activeItemStyle?: {};
+      separatorClassName?: string;
+      separatorStyle?: {};
+      separator?: React.ReactElement<any>;
+      transparentBackground?: boolean;
+      customBackgroundClassName?: string;
+      customBackgroundStyle?: {};
+      customBackground?: React.ReactElement<any>;
+      currentStep: string;
+      steps: BreadcrumbStep[];
+    }
+    interface BreadcrumbsContext {
+      muiTheme?: any;
+    }
+    export class Breadcrumbs extends React.PureComponent<BreadcrumbsProps, any> {
+      public static defaultProps: BreadcrumbsProps;
+      public static context: BreadcrumbsContext;
+      public static contextTypes: any;
+    }
   }
 }
